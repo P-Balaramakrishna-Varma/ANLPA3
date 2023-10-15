941e4352ae22b68266356eeb7c22fc4ed2960c3b
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2023-10-15 16:32:23 +0530

    coded and checked transformer

--- transformer.py
+++ transformer.py
@@ -68,10 +68,10 @@ class EncoderBlock(nn.Module):
 
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, vocab_size, embed_dim, num_layers=2, expansion_factor=4, n_heads=8):
+    def __init__(self, vocab_size, embed_dim, num_layers=2, expansion_factor=4, n_heads=8, device='cuda'):
         super().__init__()
         self.embedding_layer = GlobalEmbedding(vocab_size, embed_dim)
-        self.positional_encoder = PositionalEmbedding(embed_dim)
+        self.positional_encoder = PositionalEmbedding(embed_dim, device=device)
         self.layers = nn.ModuleList([EncoderBlock(embed_dim, expansion_factor, n_heads) for _ in range(num_layers)])
     
     def forward(self, x):
@@ -105,7 +105,7 @@ class DecoderBlock(nn.Module):
         self.dropout3 = nn.Dropout(0.2)
      
     def forward(self, query, key, val, mask):
-        self_attention_out, _ = self.attention(query, query, query, mask=mask)  
+        self_attention_out, _ = self.self_attention(query, query, query, attn_mask=mask)  
         self_attention_residual_out = self_attention_out + query  
         norm1_out = self.dropout1(self.norm1(self_attention_residual_out)) 
 
@@ -116,15 +116,15 @@ class DecoderBlock(nn.Module):
 
         feed_fwd_out = self.feed_forward(norm2_out)
         feed_fwd_residual_out = feed_fwd_out + norm2_out 
-        norm3_out = self.dropout2(self.norm2(feed_fwd_residual_out)) 
+        norm3_out = self.dropout3(self.norm3(feed_fwd_residual_out)) 
         return norm3_out
 
 
 class TransformerDecoder(nn.Module):
-    def __init__(self, target_vocab_size, embed_dim, num_layers=2, expansion_factor=2, n_heads=8):
+    def __init__(self, target_vocab_size, embed_dim, num_layers=2, expansion_factor=2, n_heads=8, device='cuda'):
         super().__init__()
         self.word_embedding = nn.Embedding(target_vocab_size, embed_dim)
-        self.position_embedding = PositionalEmbedding(embed_dim)
+        self.position_embedding = PositionalEmbedding(embed_dim, device=device)
         self.layers = nn.ModuleList([DecoderBlock(embed_dim, expansion_factor, n_heads) for _ in range(num_layers)])
         self.fc_out = nn.Linear(embed_dim, target_vocab_size)
         self.softmax = nn.Softmax(dim=2)
@@ -142,6 +142,35 @@ class TransformerDecoder(nn.Module):
         return pred
 
 
+def make_trg_mask(trg, num_heads):
+    batch_size, trg_len = trg.shape
+    trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(
+        batch_size, trg_len, trg_len
+    )
+    result = torch.cat([trg_mask] * num_heads, dim=0)
+    return result 
+
+
+
+# Transformer
+class Transformer(nn.Module):
+    def __init__(self, embed_dim, src_vocab_size, target_vocab_size,num_layers=2, expansion_factor=4, n_heads=8, device='cuda'):
+        super().__init__()
+        self.num_heads = n_heads
+        self.device = device
+        self.target_vocab_size = target_vocab_size
+        self.encoder = TransformerEncoder(src_vocab_size, embed_dim, num_layers=num_layers, expansion_factor=expansion_factor, n_heads=n_heads, device=device)
+        self.decoder = TransformerDecoder(target_vocab_size, embed_dim, num_layers=num_layers, expansion_factor=expansion_factor, n_heads=n_heads, device=device)
+    
+    def forward(self, src, trg):
+        trg_mask = make_trg_mask(trg, self.num_heads).to(self.device)
+        enc_out = self.encoder(src)
+        outputs = self.decoder(trg, enc_out, trg_mask)
+        return outputs
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -150,12 +179,31 @@ if __name__ == '__main__':
     vocab_fr = build_vocab_from_iterator(vocab_iterator("fr"), specials=["<pad>", "<unk>", "<sot>", "<eot>"], min_freq=2)
     vocab_fr.set_default_index(vocab_fr["<unk>"])
 
+    model = Transformer(300, len(vocab_en), len(vocab_fr), num_layers=2, expansion_factor=4, n_heads=3).to('cuda')
+    print(model)
     
-    enocder = TransformerEncoder(len(vocab_en), 300, 2, 2, 3).to('cuda')    
-    print(enocder)
     data = EN_Fr_Dataset('test', vocab_en, vocab_fr)
-    dataloder = torch.utils.data.DataLoader(data, batch_size=32, shuffle=False, collate_fn=custom_collate)
-    for x1, x2, y in tqdm(dataloder):
-        x1 = x1.to('cuda')
-        out = enocder(x1)
+    dataloder = torch.utils.data.DataLoader(data, batch_size=8, shuffle=False, collate_fn=custom_collate)
+    for x1, x2, y in tqdm(dataloder): 
+       x1, x2, y = x1.to('cuda'), x2.to('cuda'), y.to('cuda')
+       model(x1, x2)
+
+
+    # enocder = TransformerEncoder(len(vocab_en), 300, 2, 2, 3).to('cuda')    
+    # print(enocder)
+    # decoder = TransformerDecoder(len(vocab_fr), 300, 2, 2, 3).to('cuda')    
+    # print(decoder)
     
+    # data = EN_Fr_Dataset('train', vocab_en, vocab_fr)
+    # dataloder = torch.utils.data.DataLoader(data, batch_size=8, shuffle=False, collate_fn=custom_collate)
+    # for x1, x2, y in tqdm(dataloder):
+    #     x1 = x1.to('cuda')
+    #     out = enocder(x1)
+        
+    #     x2 = x2.to('cuda')
+    #     mask = make_trg_mask(x2, 3).to('cuda')
+    #     d_out = decoder(x2, out, mask)
+    #     # print(x2)
+    #     # print(mask)
+    #     # print("\n\n\n\n\n")
+    #     # print(x2.shape, make_trg_mask(x2).shape)
