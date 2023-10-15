6b1e5ef5bb933e0747f830a938c2c1e514df7d0f
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2023-10-15 13:22:42 +0530

    Code for decoder stack written

--- transformer.py
+++ transformer.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from data import *
 from tqdm import tqdm
 
+
 # Embedding
 class GlobalEmbedding(nn.Module):
     def __init__(self, vocab_size, embedding_dim):
@@ -13,7 +14,7 @@ class GlobalEmbedding(nn.Module):
         return self.embedding(x)
 
 
-class PostionalEmbedding(nn.Module):
+class PositionalEmbedding(nn.Module):
     def __init__(self, d_model, max_length=800, device='cuda'):
         super().__init__()
         self.pe = gen_pe(max_length, d_model).to(device)
@@ -34,6 +35,7 @@ def gen_pe(max_length, d_model):
             pe[k, 2*i+1] = math.cos(theta)
 
     return pe
+
 
 
 # Encoder
@@ -69,7 +71,7 @@ class TransformerEncoder(nn.Module):
     def __init__(self, vocab_size, embed_dim, num_layers=2, expansion_factor=4, n_heads=8):
         super().__init__()
         self.embedding_layer = GlobalEmbedding(vocab_size, embed_dim)
-        self.positional_encoder = PostionalEmbedding(embed_dim)
+        self.positional_encoder = PositionalEmbedding(embed_dim)
         self.layers = nn.ModuleList([EncoderBlock(embed_dim, expansion_factor, n_heads) for _ in range(num_layers)])
     
     def forward(self, x):
@@ -80,8 +82,64 @@ class TransformerEncoder(nn.Module):
             out = layer(embed_out)
         return out  
     
+
     
 # Decoder
+class DecoderBlock(nn.Module):
+    def __init__(self, embed_dim, expansion_factor=2, n_heads=8):
+        super().__init__()
+        self.self_attention = nn.MultiheadAttention(embed_dim, n_heads, batch_first=True)
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.dropout1 = nn.Dropout(0.2)
+        
+        self.cross_attention = nn.MultiheadAttention(embed_dim, n_heads, batch_first=True)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        self.dropout2 = nn.Dropout(0.2)
+        
+        self.feed_forward = nn.Sequential(
+                          nn.Linear(embed_dim, expansion_factor * embed_dim),
+                          nn.ReLU(),
+                          nn.Linear(expansion_factor * embed_dim, embed_dim)
+        )
+        self.norm3 = nn.LayerNorm(embed_dim)
+        self.dropout3 = nn.Dropout(0.2)
+     
+    def forward(self, query, key, val, mask):
+        self_attention_out, _ = self.attention(query, query, query, mask=mask)  
+        self_attention_residual_out = self_attention_out + query  
+        norm1_out = self.dropout1(self.norm1(self_attention_residual_out)) 
+
+
+        cross_attention_out, _ = self.cross_attention(norm1_out, key, val)
+        corss_attention_residual_out = cross_attention_out + norm1_out
+        norm2_out = self.dropout2(self.norm2(corss_attention_residual_out))
+
+        feed_fwd_out = self.feed_forward(norm2_out)
+        feed_fwd_residual_out = feed_fwd_out + norm2_out 
+        norm3_out = self.dropout2(self.norm2(feed_fwd_residual_out)) 
+        return norm3_out
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, target_vocab_size, embed_dim, num_layers=2, expansion_factor=2, n_heads=8):
+        super().__init__()
+        self.word_embedding = nn.Embedding(target_vocab_size, embed_dim)
+        self.position_embedding = PositionalEmbedding(embed_dim)
+        self.layers = nn.ModuleList([DecoderBlock(embed_dim, expansion_factor, n_heads) for _ in range(num_layers)])
+        self.fc_out = nn.Linear(embed_dim, target_vocab_size)
+        self.softmax = nn.Softmax(dim=2)
+        # Possible to add a dropout layer here
+
+    def forward(self, x, enc_out, mask):
+        global_embd_out = self.word_embedding(x)
+        position_embd_out = self.position_embedding(x)
+        out = global_embd_out + position_embd_out
+        
+        for layer in self.layers:
+            out = layer(out, enc_out, enc_out, mask) 
+        
+        pred = self.softmax(self.fc_out(out))
+        return pred
 
 
 
